4894e10e27a657378f024152bf5a2eb14dc080ba
maxigarrett <maxyg22@outlook.es>
2025-03-11 11:50:48 -0300

    agregado de funciones nuevas usememo,usecallbac y debounce

--- 06-prueba-tecnica-app-peliculas/src/App.jsx
+++ 06-prueba-tecnica-app-peliculas/src/App.jsx
@@ -1,19 +1,35 @@
-// import { useEffect, useState } from "react";
+import { useCallback, useState } from "react";
 import "./App.css";
 import { Movies } from "./components/Movies";
 import { useMovies } from "./hooks/useMovies";
 import { useSearch } from "./hooks/useSearch";
-
+import { OrbitProgress } from "react-loading-indicators";
+import debounce from "just-debounce-it";
 function App() {
-  const { search, error, updateSearch } = useSearch();
-  const { movies, getMovies } = useMovies({ search });
+  const [sort, setSort] = useState(false);
+  //solo actualizamos el serach de este ook para los mensajes de error ya q elsearch que lepasamos a los hooks se lo pasamos por parametros
+  //a las funciones de los hooks que tienen internamente en este caso getMoives que le pasasmos el valor del input para hacer peticiones a la API
+  const { error, updateSearch } = useSearch();
+  const { movies, getMovies, loading } = useMovies({ sort });
 
+  //este debounce es para que espere unos milisegundos antes de hacer la busqueda en el onchange porque
+  //al ir escribiendo se van realizando peticiones y puede que una se resuelva mas rapido que otra y traiga resultados diferentes
+  //por eso hacemos que espere unos segundos antes de hacer la busqueda y solo buscara lo ultimo que tipeo en el input del usuario
+  // y usamos un usecallback para que recuerde esta funcion y no renderice constantemente ya que cada tipeo que icimos
+  //las buscara de golpe luego de esos ms de espera, por eso usamos el usecallback para que no renderice el mismo compoenente
+  //cada vez que tipeamos
+  const debounceGetMovies = useCallback(
+    debounce((search) => {
+      getMovies({ search });
+    }, 300),
+    [getMovies]
+  );
   //si hay un error agrega una clase al input
   const classNameInput = error ? "errorSearch" : "";
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    getMovies();
+    const newQuery = event.target[0].value;
     // AL GET LE PASAMOS EL NAME DEL InPUT
     // const fields = new window.FormData(event.target);
     // const query = fields.get("queryMovie");
@@ -25,12 +41,20 @@ function App() {
     // const { queryMovie } = Object.fromEntries(
     //   new window.FormData(event.target)
     // );
+    // updateSearch(queryMovie)
+    getMovies({ search: newQuery });
   };
 
   const handleChange = (e) => {
     //es mejor guardar primero  en variable el valor ya que es asincrono puede traer errores o bugs.
     const newQuery = e.target.value;
-    updateSearch(newQuery); //actualiza el  earch que tenemos en el hook para mostrar mensaje
+    updateSearch(newQuery); //actualiza el search que tenemos en el hook para mostrar mensaje
+    debounceGetMovies(newQuery);
+  };
+
+  const handleSort = () => {
+    setSort((sort) => !sort);
+    console.log(sort);
   };
 
   return (
@@ -45,15 +69,25 @@ function App() {
             name="queryMovie"
             type="text"
             placeholder="....search movies"
-            value={search}
           />
+          <input type="checkbox" onChange={handleSort} checked={sort} />
           <button type="submit">Searc</button>
         </form>
       </header>
       {error && <span className="errorSearch">{error}</span>}
 
       <main>
-        <Movies movies={movies} />
+        {loading ? (
+          <OrbitProgress
+            dense
+            color="azure"
+            size="medium"
+            text=""
+            textColor=""
+          />
+        ) : (
+          <Movies movies={movies} />
+        )}
       </main>
     </div>
   );
--- 06-prueba-tecnica-app-peliculas/src/hooks/useMovies.js
+++ 06-prueba-tecnica-app-peliculas/src/hooks/useMovies.js
@@ -1,14 +1,47 @@
-import { useState } from "react";
+import { useRef, useState ,useMemo,useCallback} from "react";
 import { searchMovies } from "../services/movies";
+  //USEMEMO si buscamosuna pelicula y ordenamos por anio y borramos en el buscador no queremosque se renderice
+  //de nuevo el sorted o movies porque no estamos haciendo nada mas que cambiar el valor del search
+  //pero si le damos a buscar o ordenar es normal que se ejecute.
 
-
-
-export const useMovies = ({search}) => {
+  //usecallback es lo mismo que useMemo pero solo que esta pensado para funciones y nos ahorramos la funcion anonima que va dentro
+  //y es mas legible y se la pnemos a la fuuncion getmovies para evitar que se renderice que le damos a buscar o cliqueamos en el checkbox
+  //solo se renderiza cuando escribimos en el  input
+  //si le pasamos al usecallback el search al getMoives y no al customhook en general optimizaremos mas ya que no se rendizara mientras 
+  //escribamos tampoco y necesitamos sacarle la dependecia de abajo y dejarla vacia ya q no la necesitamos solo queremos que
+  //se renderize una vez y ya
+export const useMovies = ({sort}) => {
   const[movies,setMovies]=useState([]);
+  const[loading,setLoading]=useState(false);
+  const[error,setError]=useState(null);
+  const previusSearch=useRef('');
 
-  const getMovies= async()=>{
-    const newMoives=await searchMovies({search})
-    setMovies(newMoives)
-  }
-  return {movies ,getMovies};
+  const getMovies= useCallback(
+    async({search})=>{
+      //evitar la misma busqueda
+      if(search=== previusSearch.current) return
+      try {
+        setLoading(true)
+        previusSearch.current=search
+        const newMoives=await searchMovies({search})
+        setMovies(newMoives)
+      } catch (error) {
+        setError(error.message)
+      }finally{
+        setLoading(false)
+      }
+    },[])
+
+
+
+
+  //si el ceckbox esta en true ordena por titulos o por año
+  // const sortedMovies= sort ? [...movies].sort((a,b)=>a.title.localeCompare(b.title)):movies
+  const sortedMovies =useMemo(()=>{
+    return sort 
+    ? [...movies].sort((a,b)=>a.year - b.year)
+    :movies
+  },[sort,movies])
+
+  return {movies: sortedMovies ,getMovies,loading,error};
 };
